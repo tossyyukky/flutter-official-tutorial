3a75739c49418d6a292dab300519218f0fb8140e
Toshiyuki Ihara <snowsphere.burned@gmail.com>
2020-05-30 12:50:05 +0900

    modify: CurvedAnimation

--- lib/main.dart
+++ lib/main.dart
@@ -19,24 +19,20 @@ class _LogoAppState extends State<LogoApp> with SingleTickerProviderStateMixin {
     super.initState();
     controller =
         AnimationController(duration: const Duration(seconds: 2), vsync: this);
-    animation = Tween<double>(begin: 0, end: 300).animate(controller)
+    animation = CurvedAnimation(parent: controller, curve: Curves.easeIn)
       ..addStatusListener((status) {
-
         if (status == AnimationStatus.completed) {
           controller.reverse();
         } else if (status == AnimationStatus.dismissed) {
           controller.forward();
         }
       })
-    ..addStatusListener((state) => print('$state'));
+      ..addStatusListener((state) => print('$state'));
     controller.forward();
   }
 
   @override
-  Widget build(BuildContext context) => GrowTransition(
-    child: LogoWidget(),
-    animation: animation,
-  );
+  Widget build(BuildContext context) => AnimatedLogo(animation: animation);
 
   @override
   void dispose() {
@@ -46,26 +42,33 @@ class _LogoAppState extends State<LogoApp> with SingleTickerProviderStateMixin {
 }
 
 class AnimatedLogo extends AnimatedWidget {
+  static final _opacityTween = Tween<double>(begin: 0.1, end: 1);
+  static final _sizeTween = Tween<double>(begin: 0, end: 300);
+
   AnimatedLogo({Key key, Animation<double> animation})
       : super(key: key, listenable: animation);
 
   Widget build(BuildContext context) {
     final animation = listenable as Animation<double>;
     return Center(
-        child: Container(
-      margin: EdgeInsets.symmetric(vertical: 10),
-      height: animation.value,
-      width: animation.value,
-      child: FlutterLogo(),
-    ));
+        child: Opacity(
+            opacity: _opacityTween.evaluate((animation)),
+            child: Container(
+              margin: EdgeInsets.symmetric(vertical: 10),
+              height: _sizeTween.evaluate(animation),
+              width: _sizeTween.evaluate(animation),
+              child: FlutterLogo(),
+            )
+        )
+    );
   }
 }
 
 class LogoWidget extends StatelessWidget {
   Widget build(BuildContext context) => Container(
-    margin: EdgeInsets.symmetric(vertical: 10),
-    child: FlutterLogo(),
-  );
+        margin: EdgeInsets.symmetric(vertical: 10),
+        child: FlutterLogo(),
+      );
 }
 
 class GrowTransition extends StatelessWidget {
@@ -75,16 +78,16 @@ class GrowTransition extends StatelessWidget {
   final Animation<double> animation;
 
   Widget build(BuildContext context) => Center(
-    child: AnimatedBuilder(
-      animation: animation,
-      builder: (context, child) => Container(
-        height: animation.value,
-        width: animation.value,
-        child: child,
-      ),
-      child: child,
-    ),
-  );
+        child: AnimatedBuilder(
+          animation: animation,
+          builder: (context, child) => Container(
+            height: animation.value,
+            width: animation.value,
+            child: child,
+          ),
+          child: child,
+        ),
+      );
 }
 
 //class MyApp extends StatelessWidget {
